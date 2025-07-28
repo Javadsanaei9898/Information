c8a21c232552d0c3b1787ccaf20bc2a6bc3fc28a
Javadsanaei9898 <javadsanaei84@gmail.com>
2025-07-28 21:31:42 +0330

    Create INFO.py

---
+++ INFO.py
@@ -0,0 +1,14 @@
+Name = input("what is your first name? ")
+Family = input("what is your last name? ")
+Age = input("how old are you? ")
+FAVcolor = input("what is your favorite color? ")
+street = input("where is your street? ")
+Gender = input("are you male or female? ")
+job = input("what is your job? ")
+birthday = input("when is your birthday? ")
+MotherName = input("what is your Mother's name? ")
+FatherName = input("what is your Father'name? ")
+
+print("Hi my name is " + Name + Family + " i am " + Age + " years old " + " my favorite color is " +
+      FAVcolor + " i live in " + street + " street " + " my gender is " + Gender + " my job is " + job + 
+      " and i born on " + birthday + " my mother's name is " + MotherName + " and my father's name is " + FatherName + " j")
